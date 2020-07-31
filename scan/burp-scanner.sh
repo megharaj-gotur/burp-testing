b8f6a1a02a07cc9842459227198b898256b123c0
mgotur <mgotur@xtivia.com>
2020-07-30 22:55:21 -0500

    test

--- scan/burp-scanner.sh
+++ scan/burp-scanner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # burp scanner script
-
+echo "entering burp script"
 ls -lrt
-java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics_scan_config.json --self-signed-cert=empty_ssl_cert.pem http://35.153.160.75:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < temp1.log 
+java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics_scan_config.json --self-signed-cert=empty_ssl_cert.pem http://35.153.160.75:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < ./scan/temp1.log 
