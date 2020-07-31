e50df96d0886c4635ec012625fe0b24c63c0287f
mgotur <mgotur@xtivia.com>
2020-07-30 22:46:35 -0500

    test

--- burp-scanner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# My first script
-
-echo "Hello World!"
-ls -lrt
-java -jar burp-ci-driver-1.0.7beta.jar http://35.153.160.75:8080/api/kGOvDZuqUsK2k19wRpkEfJ0y8JadQF7L < temp.log --self-signed-cert=empty_ssl_cert.pem
