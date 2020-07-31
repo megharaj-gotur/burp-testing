650b981046c5442f2e9409e2fe041c971f5c2264
mgotur <mgotur@xtivia.com>
2020-07-31 00:56:07 -0500

    test

--- scan/burp-scanner.sh
+++ scan/burp-scanner.sh
@@ -4,4 +4,7 @@ echo "entering burp script"
 ls -lrt
 cd scan
 ls -lrt
+while sleep 5m; do
+	echo "====[ $SECONDS seconds still running ]===="
+done
 java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics_scan_config.json --self-signed-cert=empty_ssl_cert.pem http://35.153.160.75:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < temp1.log 
