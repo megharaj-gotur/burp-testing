fe9a224eb1962dc10da901511e0cd4402eb430e7
mgotur <mgotur@xtivia.com>
2020-07-30 23:16:43 -0500

    test

--- scan/burp-scanner.sh
+++ scan/burp-scanner.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # burp scanner script
 echo "entering burp script"
-ls -lrt
-cd scan
 java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics_scan_config.json --self-signed-cert=empty_ssl_cert.pem http://35.153.160.75:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < temp1.log 
