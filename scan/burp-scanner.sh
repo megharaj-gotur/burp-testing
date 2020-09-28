85319d58f5abaf1c2c7c6ee7ebeb804698c033de
mgotur <mgotur@xtivia.com>
2020-09-27 22:23:29 -0500

    test

--- scan/burp-scanner.sh
+++ scan/burp-scanner.sh
@@ -2,4 +2,4 @@
 # burp scanner script
 echo "entering burp script"
 cd scan
-java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics_scan_config.json --self-signed-cert=empty_ssl_cert.pem http://3.90.150.214:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < sbt_build.log 
+java -jar burp-ci-driver-1.0.7beta.jar --scan-definition=localytics-scan-config.json --self-signed-cert=empty_ssl_cert.pem http://3.90.150.214:8080/api/0OGicr9p72Avkggg1IqYnmxT5PgkNR0R < sbt_build.log 
